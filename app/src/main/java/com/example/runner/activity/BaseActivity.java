e5ff189312dfd748d6288f72b3d9feac4ffcb288
Jack <qiankuo@johnsonfitness.com>
2019-04-05 19:52:42 +0800

    添加登录和注册界面

---
+++ app/src/main/java/com/example/runner/activity/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.example.runner.activity;
+
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+/**
+ * 项目中所有Activity的基类
+ * Created by liuheng on 16/6/26.
+ */
+public class BaseActivity extends FragmentActivity {
+
+    protected void showToast(String toast){
+        Toast.makeText(this,toast, Toast.LENGTH_LONG).show();
+    }
+
+}
---
+++ app/src/main/java/com/example/runner/model/entity/BaseResult.java
@@ -0,0 +1,17 @@
+package com.example.runner.model.entity;
+
+/**
+ * 所有返回的数据的基类
+ * Created by liuheng on 16/7/7.
+ */
+public class BaseResult {
+    private int result;
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+}
